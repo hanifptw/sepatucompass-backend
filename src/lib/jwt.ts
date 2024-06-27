a533fc2c321018ad5403e3b30497c27f77a91114
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-06-28 02:08:20 +0700

    feat: jwt lib handle token

---
+++ src/lib/jwt.ts
@@ -0,0 +1,48 @@
+import { HMAC } from "oslo/crypto";
+import { createJWT, validateJWT } from "oslo/jwt";
+import { TimeSpan } from "oslo";
+
+const getSecret = async () => {
+  const encoder = new TextEncoder();
+  const uint8Array = encoder.encode(process.env.TOKEN_SECRET);
+  return uint8Array.buffer as ArrayBuffer;
+};
+
+const payload = {
+  message: "hello, world",
+};
+
+export const createToken = async (subject: string) => {
+  const secret = await getSecret();
+
+  try {
+    const jwt = await createJWT("HS256", secret, payload, {
+      expiresIn: new TimeSpan(30, "d"),
+      subject,
+      includeIssuedTimestamp: true,
+    });
+
+    return jwt;
+  } catch (error) {
+    console.error();
+    
+    return;
+  }
+};
+
+export const validateToken = async (token: string) => {
+  const secret = await getSecret();
+  try {
+    const result = validateJWT("HS256", secret, token);
+    // const message = jwt.payload.message;
+
+    return result;
+  } catch (error) {
+    console.error();
+    return null;
+
+    // invalid signature
+    // expired token
+    // inactive token (`nbf`)
+  }
+};
